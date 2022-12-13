42f974c8b56b591d72babfe4cb36f45ef9ceb338
merf-ai <nik.bolshakov.14@bk.ru>
2022-12-13 23:29:40 +0300

    test

--- backend/site/users/views.py
+++ backend/site/users/views.py
@@ -8,11 +8,7 @@ from .serializers import UserSerializer
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.authentication import TokenAuthentication
-from django.contrib.auth import login, authenticate
-from rest_framework.authentication import SessionAuthentication, BasicAuthentication
 from rest_framework.permissions import IsAuthenticated
-from django.views.generic.base import View
-from django.shortcuts import render
 from rest_framework.authtoken.models import Token
 
 # Create your views here.
